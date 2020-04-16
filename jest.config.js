2e7770bd7f476628909c9406a5979f7635a07211
Aaron <aaron.harper@outlook.com>
2020-04-15 20:15:21 -0400

    Mock css imports

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   moduleFileExtensions: ["js", "json", "vue"],
+  moduleNameMapper: {
+    "\\.(css)$": "<rootDir>/__mocks__/styleMock.js",
+  },
   transform: {
     ".*\\.(js)$": "babel-jest",
     ".*\\.(vue)$": "vue-jest",
