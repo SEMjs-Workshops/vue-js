12c409b7d2d3ba6994232f6a88142000791778b8
Aaron Harper <aharper@duo.com>
2020-05-10 18:09:00 -0400

    lab-template-syntax

--- src/lab-template-syntax/final/App.vue
+++ src/lab-template-syntax/final/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <div>{{ appName }}</div>
+    <h1>{{ appName }}</h1>
+    <div>{{ completedTodosCount }} todos completed</div>
   </div>
 </template>
 
@@ -8,12 +9,17 @@
 export default {
   data() {
     return {
-      appName: "app name here",
+      appName: "Todo-aloo",
       todos: [
         { isComplete: false, text: "Call mom" },
         { isComplete: true, text: "Buy groceries" },
       ],
     };
+  },
+  computed: {
+    completedTodosCount: function() {
+      return this.todos.filter((todo) => todo.isComplete).length;
+    },
   },
 };
 </script>
---
+++ src/lab-template-syntax/start/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ appName }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      appName: "app name here",
+      todos: [
+        { isComplete: false, text: "Call mom" },
+        { isComplete: true, text: "Buy groceries" },
+      ],
+    };
+  },
+};
+</script>
